370f6db0756f1ab10c275064c42fe9175af14148
Gwen Hey <itsgwenhey@gmail.com>
2019-08-31 21:02:02 -0500

    finished working resetable customizeable

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,12 +1,33 @@
+let gridSize;
 
+const reset = () =>{
+    $('#gridHolder').empty();
+
+    let num = window.prompt('How many pixels on each side of the grid?');
+
+    gridSize = num * num;
+
+    let gridWidth = 100/num;
+
+    let gridString = gridWidth + '%';
+    
+    for(let i=0; i < gridSize; i++){
+        $('#gridHolder').append('<div class="gridBox"></div>');
+        console.log(i);
+    }
+
+    $('.gridBox').css('width',gridString);
+
+    $('.gridBox').on('mouseenter', () => {
+        event.target.style.backgroundColor = 'red';
+    })
+}
 
 for(let i=0; i < 256; i++){
     $('#gridHolder').append('<div class="gridBox"></div>');
     console.log(i);
 }
-
-let gridBox = document.getElementById('gridBox');
-
+ 
 $('.gridBox').on('mouseenter', () => {
     event.target.style.backgroundColor = 'red';
 })
