2e307a2965245c7867e4f1a11bae217e960d70ff
Gwen Hey <itsgwenhey@gmail.com>
2019-08-31 20:06:32 -0500

    grid and drawing works

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,4 +1,13 @@
-for(let i=0; i < 16; i++){
+
+
+for(let i=0; i < 256; i++){
     $('#gridHolder').append('<div class="gridBox"></div>');
     console.log(i);
 }
+
+let gridBox = document.getElementById('gridBox');
+
+$('.gridBox').on('mouseenter', () => {
+    event.target.style.backgroundColor = 'red';
+})
+
